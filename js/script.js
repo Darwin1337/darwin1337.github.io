0ded401a1dd48837071cbe54219576ced2996d36
Diogo Borges <40754665+Darwin1337@users.noreply.github.com>
2020-10-10 15:26:23 +0100

    updated website
    added navigation with images

---
+++ js/script.js
@@ -0,0 +1,28 @@
+setInterval(function() {
+  if ($(".image-um").is(":hover") || $(".fase-um").is(":hover")) {
+    console.log("yes!")
+    $(".fase-um p").css("background", "white");
+    $(".fase-um p").css("color", "rgba(0, 0, 0, 0.5)");
+    $(".image-um").css("filter", "blur(6px)");
+    $(".image-um").css("-webkit-filter", "blur(6px)");
+    $(".fase-um").css("cursor", "pointer");
+  } else {
+    $(".fase-um p").css("background", "none");
+    $(".fase-um p").css("color", "white");
+    $(".image-um").css("filter", "blur(2px)");
+    $(".image-um").css("-webkit-filter", "blur(2px)");
+  }
+  if ($(".image-dois").is(":hover") || $(".fase-dois").is(":hover")) {
+    console.log("yes!")
+    $(".fase-dois p").css("background", "white");
+    $(".fase-dois p").css("color", "rgba(0, 0, 0, 0.5)");
+    $(".image-dois").css("filter", "blur(6px)");
+    $(".image-dois").css("-webkit-filter", "blur(6px)");
+    $(".fase-dois").css("cursor", "pointer");
+  } else {
+    $(".fase-dois p").css("background", "none");
+    $(".fase-dois p").css("color", "white");
+    $(".image-dois").css("filter", "blur(2px)");
+    $(".image-dois").css("-webkit-filter", "blur(2px)");
+  }
+}, 200);

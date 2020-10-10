9c5d470b13b08fafc5c2cde4c9573b390364349d
Diogo Borges <40754665+Darwin1337@users.noreply.github.com>
2020-10-11 00:52:56 +0100

    small design errors fixed

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 setInterval(function() {
   if ($(".image-um").is(":hover") || $(".fase-um").is(":hover")) {
     $(".fase-um p").css("background", "white");
-    $(".fase-um p").css("color", "rgba(0, 0, 0, 0.5)");
+    $(".fase-um p a").css("color", "rgba(0, 0, 0, 0.5)");
     $(".image-um").css("filter", "blur(6px)");
     $(".image-um").css("-webkit-filter", "blur(6px)");
-    $(".fase-um").css("cursor", "pointer");
+    $(".fase-um p").css("cursor", "pointer");
   } else {
     $(".fase-um p").css("background", "none");
-    $(".fase-um p").css("color", "white");
+    $(".fase-um p a").css("color", "white");
     $(".image-um").css("filter", "blur(2px)");
     $(".image-um").css("-webkit-filter", "blur(2px)");
   }
   if ($(".image-dois").is(":hover") || $(".fase-dois").is(":hover")) {
     $(".fase-dois p").css("background", "white");
-    $(".fase-dois p").css("color", "rgba(0, 0, 0, 0.5)");
+    $(".fase-dois p a").css("color", "rgba(0, 0, 0, 0.5)");
     $(".image-dois").css("filter", "blur(6px)");
     $(".image-dois").css("-webkit-filter", "blur(6px)");
-    $(".fase-dois").css("cursor", "pointer");
+    $(".fase-dois p").css("cursor", "pointer");
   } else {
     $(".fase-dois p").css("background", "none");
-    $(".fase-dois p").css("color", "white");
+    $(".fase-dois p a").css("color", "white");
     $(".image-dois").css("filter", "blur(2px)");
     $(".image-dois").css("-webkit-filter", "blur(2px)");
   }

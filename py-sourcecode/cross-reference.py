82bae5b9e682b47168dd0d67e62fe0225a365639
Diogo Borges <40754665+Darwin1337@users.noreply.github.com>
2020-10-11 15:00:44 +0100

    removed useless imports

--- py-sourcecode/cross-reference.py
+++ py-sourcecode/cross-reference.py
@@ -2,10 +2,6 @@ import os
 import time
 import json
 import traceback
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 
 def clear():

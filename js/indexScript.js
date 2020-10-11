01f999301dc30978f1cc483d87b2573c95b68565
Diogo Borges <40754665+Darwin1337@users.noreply.github.com>
2020-10-11 14:22:31 +0100

    update to a more simpler design

---
+++ js/indexScript.js
@@ -0,0 +1,15 @@
+$("#bt-um").click(function(event) {
+  window.location.href = "primeira_fase";
+  return false;
+});
+
+$("#bt-dois").click(function(event) {
+  window.location.href = "segunda_fase";
+  return false;
+});
+
+
+$("#bt-tres").click(function(event) {
+  window.location.href = "terceira_fase";
+  return false;
+});

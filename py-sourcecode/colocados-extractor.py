04b9c75bc6e67bc6108eb0bccadebf80ecb85e8c
Diogo Borges <40754665+Darwin1337@users.noreply.github.com>
2020-10-11 14:58:44 +0100

    added relevant sourcecode

---
+++ py-sourcecode/colocados-extractor.py
@@ -0,0 +1,63 @@
+import os
+import time
+import json
+import traceback
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def pause():
+    os.system("pause")
+
+DriverPath = r"C:\Users\diogo\Desktop\chromedriver.exe"
+driver = webdriver.Chrome(DriverPath)
+
+link = ["https://www.dges.gov.pt/coloc/2020/col1listas.asp?CodR=11&action=2", "https://www.dges.gov.pt/coloc/2020/col1listas.asp?CodR=12&action=2"]
+print("[" + datetime.now().strftime("%H:%M:%S") + "] Script iniciado\n")
+JSONarray = []
+
+# [Começo] Código para os colocados
+
+try:
+    for p in range(2):
+        driver.get(link[p])
+        for x in range(len(driver.find_elements_by_class_name("inputtext > option"))):
+            JSONindex = 0
+            JSONcontent = ""
+            JSONcontent = { "estabelecimento": str(driver.find_elements_by_class_name("inputtext > option")[x].get_attribute('innerHTML')[7:]), "codigo_estabelecimento": str(driver.find_elements_by_class_name("inputtext > option")[x].get_attribute('value')) }
+            print("Faculdade: " + str(driver.find_elements_by_class_name("inputtext > option")[x].get_attribute('innerHTML')[7:]) + "\n")
+            Select(driver.find_elements_by_class_name('inputtext')[0]).select_by_value(driver.find_elements_by_class_name("inputtext > option")[x].get_attribute('value'))
+            driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/form/table[2]/tbody/tr[2]/td/input[4]").click()
+            JSONcontent['data'] = []
+            for j in range(len(driver.find_elements_by_class_name("inputtext > option"))):
+                if "Engenharia Alimentar" in str(driver.find_elements_by_class_name("inputtext > option")[j].get_attribute('innerHTML')[7:].replace("\n", "")):
+                    print("Curso: " + driver.find_elements_by_class_name("inputtext > option")[j].get_attribute('innerHTML')[7:].replace("\n", ""))
+                    JSONcontent['data'].append({ "curso": driver.find_elements_by_class_name("inputtext > option")[j].get_attribute('innerHTML')[7:].replace("\n", ""), "codigo_curso": driver.find_elements_by_class_name("inputtext > option")[j].get_attribute('value') })
+                    Select(driver.find_elements_by_class_name('inputtext')[0]).select_by_value(driver.find_elements_by_class_name("inputtext > option")[j].get_attribute('value'))
+                    driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/form/table[2]/tbody/tr[2]/td/input[3]").click()
+                    JSONcontent['data'][int(JSONindex)]['colocados'] = []
+                    time.sleep(1)
+                    print("Encontrados " + str(len(driver.find_elements_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[3]/tbody/tr"))) + " alunos colocados neste curso\n")
+                    for c in range(len(driver.find_elements_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[3]/tbody/tr"))):
+                        JSONcontent['data'][int(JSONindex)]['colocados'].append({ "nome": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[3]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[1].get_attribute('innerHTML').replace("/n", "")) })
+                    driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/a").click()
+                    JSONindex = JSONindex + 1
+            JSONarray.append(JSONcontent)
+            driver.get(link[p])
+    print("A guardar ficheiro com a informação...")
+    with open(r'C:\Users\diogo\Desktop\teste.json', 'w') as f:
+        f.write(json.dumps(JSONarray))
+    driver.quit()
+    print("[" + datetime.now().strftime("%H:%M:%S") + "] Script concluído com sucesso.")
+except Exception as e:
+    print(str(e))
+    traceback.print_exc()
+
+# [Fim] Código para os colocados
+
+pause()
---
+++ py-sourcecode/cross-reference.py
@@ -0,0 +1,66 @@
+import os
+import time
+import json
+import traceback
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def pause():
+    os.system("pause")
+
+# [Começo] Código para a cross reference
+
+with open(r"C:\Users\diogo\Desktop\Lista Candidatos.json", encoding = "utf8") as json_file1:
+    candidatos = json.load(json_file1)
+with open(r"C:\Users\diogo\Desktop\Lista Colocados.json", encoding = "utf8") as json_file2:
+    colocados = json.load(json_file2)
+
+print("[" + datetime.now().strftime("%H:%M:%S") + "] Script iniciado\n")
+JSONarray = []
+TotalAlunos = 0
+
+try:
+    for x in range(len(colocados)):
+        JSONindex = 0
+        JSONcontent = ""
+        JSONcontent = { "estabelecimento": str(colocados[x]['estabelecimento']), "codigo_estabelecimento": str(colocados[x]['codigo_estabelecimento']) }
+        JSONcontent['data'] = []
+        for j in range(len(colocados[x]['data'])):
+            JSONcontent['data'].append({ "curso": str(colocados[x]['data'][j]['curso']), "codigo_curso": str(colocados[x]['data'][j]['codigo_curso']) })
+            JSONcontent['data'][int(JSONindex)]['colocados'] = []
+            for k in range(len(colocados[x]['data'][j]['colocados'])):
+                AlunoEncontrado = False
+                TotalAlunos = TotalAlunos + 1
+                print("Aluno " + str(TotalAlunos) + " de 51964")
+                print("Nome: " + str(colocados[x]['data'][j]['colocados'][k]['nome']))
+                # Para cada aluno, encontrar a sua nota de candidatura
+                for i in range(len(candidatos)):
+                    for n in range(len(candidatos[i]['data'])):
+                        for m in range(len(candidatos[i]['data'][n]['candidatos'])):
+                            if str(candidatos[i]['data'][n]['candidatos'][m]['nome']) == str(colocados[x]['data'][j]['colocados'][k]['nome']):
+                                if str(candidatos[i]['estabelecimento']) == str(colocados[x]['estabelecimento']):
+                                    if str(candidatos[i]['data'][n]['curso']) == str(colocados[x]['data'][j]['curso']):
+                                        AlunoEncontrado = True
+                                        print("Sucesso!")
+                                        JSONcontent['data'][int(JSONindex)]['colocados'].append({ "nome": str(candidatos[i]['data'][n]['candidatos'][m]['nome']), "nota": str(candidatos[i]['data'][n]['candidatos'][m]['nota']), "opcao": str(candidatos[i]['data'][n]['candidatos'][m]['opcao']) })
+            if AlunoEncontrado == False:
+                print("Erro! Aluno não encontrado!")
+                time.sleep(10)
+            JSONindex = JSONindex + 1
+        JSONarray.append(JSONcontent)
+    with open(r'C:\Users\diogo\Desktop\final.json', 'w') as f:
+        f.write(json.dumps(JSONarray))
+except Exception as e:
+    print(str(e))
+    traceback.print_exc()
+print("[" + datetime.now().strftime("%H:%M:%S") + "] Script concluído com sucesso.")
+
+# [Fim] Código para a cross reference
+
+pause()

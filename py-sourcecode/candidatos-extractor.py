523142cb1bc391cfa0620a17fa6e42a8546d4a63
Diogo Borges <40754665+Darwin1337@users.noreply.github.com>
2020-10-15 19:55:21 +0100

    update to 2nd fase of applications

--- py-sourcecode/candidatos-extractor.py
+++ py-sourcecode/candidatos-extractor.py
@@ -17,7 +17,7 @@ def pause():
 DriverPath = r"C:\Users\diogo\Desktop\chromedriver.exe"
 driver = webdriver.Chrome(DriverPath)
 
-link = ["https://www.dges.gov.pt/coloc/2020/col1listas.asp?CodR=11&action=2", "https://www.dges.gov.pt/coloc/2020/col1listas.asp?CodR=12&action=2"]
+link = ["https://www.dges.gov.pt/coloc/2020/col2listas.asp?CodR=11&action=2", "https://www.dges.gov.pt/coloc/2020/col2listas.asp?CodR=12&action=2"]
 print("[" + datetime.now().strftime("%H:%M:%S") + "] Script iniciado\n")
 JSONarray = []
 
@@ -47,7 +47,7 @@ try:
                 if driver.find_elements_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a"):
                     MaisQueUmaPagina = True
                     print("O curso tem mais que 1 página de candidatos, a gerar URL...")
-                    URLGerado = "https://www.dges.gov.pt/coloc/2020/col1listaser.asp?CodEstab=" + str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a").get_attribute("href").split("=")[1].split("&")[0]) + "&CodCurso=" + str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a").get_attribute("href").split("=")[2].split("&")[0]) + "&ids=1&ide=" + str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a").get_attribute("href").split("=")[5]) + "&Mx=" + str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a").get_attribute("href").split("=")[5])
+                    URLGerado = "https://www.dges.gov.pt/coloc/2020/col2listaser.asp?CodEstab=" + str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a").get_attribute("href").split("=")[1].split("&")[0]) + "&CodCurso=" + str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a").get_attribute("href").split("=")[2].split("&")[0]) + "&ids=1&ide=" + str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a").get_attribute("href").split("=")[5]) + "&Mx=" + str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[2]/tbody/tr/td/a").get_attribute("href").split("=")[5])
                     print("URL gerado: " + URLGerado)
                     driver.execute_script('''window.open("","_blank");''')
                     driver.switch_to.window(driver.window_handles[1])
@@ -64,7 +64,14 @@ try:
                         opcao = "1"
                     else:
                         opcao = str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[4].get_attribute('innerHTML').replace("/n", "").strip())
-                    JSONcontent['data'][int(JSONindex)]['candidatos'].append({ "nome": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[2].get_attribute('innerHTML').replace("/n", "").strip()), "nota": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[3].get_attribute('innerHTML').replace("/n", "").strip()), "opcao": opcao, "prova_ingresso": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[5].get_attribute('innerHTML').replace("/n", "").strip()), "nota_12ano": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[6].get_attribute('innerHTML').replace("/n", "").strip()), "nota_10ano_11ano": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[7].get_attribute('innerHTML').replace("/n", "").strip()) })
+                    JSONcontent['data'][int(JSONindex)]['candidatos'].append({
+                    "nome": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[2].get_attribute('innerHTML').replace("/n", "").strip()),
+                    "cc": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[1].get_attribute('innerHTML').replace("/n", "").strip()),
+                    "nota": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[3].get_attribute('innerHTML').replace("/n", "").strip()),
+                    "opcao": opcao, "prova_ingresso": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[5].get_attribute('innerHTML').replace("/n", "").strip()),
+                    "nota_12ano": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[6].get_attribute('innerHTML').replace("/n", "").strip()),
+                    "nota_10ano_11ano": str(driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td/div[2]/table[4]/tbody/tr[%s]" % (c + 1)).find_elements_by_tag_name('td')[7].get_attribute('innerHTML').replace("/n", "").strip())
+                    })
                 if MaisQueUmaPagina == True:
                     print("Como há mais que uma página, teremos de fechar a aba aberta")
                     driver.close()
--- py-sourcecode/get-options.py
+++ py-sourcecode/get-options.py
@@ -2,6 +2,10 @@ import os
 import time
 import json
 import traceback
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 
 def clear():
@@ -18,10 +22,9 @@ with open(r"C:\Users\diogo\Desktop\CNAES 2020\Lista Colocados2.json", encoding =
     colocados = json.load(json_file2)
 
 class Aluno:
-    def __init__(self, nome, nota_12ano, nota_10ano_11ano):
+    def __init__(self, nome, opcao):
         self.Nome = nome
-        self.Nota_12ano = nota_12ano
-        self.Nota_10ano_11ano = nota_10ano_11ano
+        self.Opcao = opcao
 
 start = "[" + datetime.now().strftime("%H:%M:%S") + "] Script iniciado\n"
 JSONarray = []
@@ -45,30 +48,40 @@ try:
                     JSONcontent = ""
                     JSONcontent = { "nome": str(candidatos[x]['data'][j]['candidatos'][k]['nome']), "nota_10ano_11ano": str(candidatos[x]['data'][j]['candidatos'][k]['nota_10ano_11ano']), "nota_12ano": str(candidatos[x]['data'][j]['candidatos'][k]['nota_12ano']) }
                     JSONcontent['opcoes'] = []
+                    MesmoNome = []
                     for c in range(len(candidatos)):
                         for p in range(len(candidatos[c]['data'])):
                             for i in range(len(candidatos[c]['data'][p]['candidatos'])):
-                                # Verificar se existem opções duplicadas
-                                # Se existir abrir a página de candidatos e obter os números de CC e filtrar alunos por esse número
-                                colocado = "false"
-                                    if not EncontrouColocado:
-                                        for r in range(len(colocados)):
-                                            for t in range(len(colocados[r]['data'])):
-                                                for y in range(len(colocados[r]['data'][t]['colocados'])):
-                                                    if str(candidatos[x]['data'][j]['candidatos'][k]['nome']) == str(colocados[r]['data'][t]['colocados'][y]['nome']):
-                                                        if str(candidatos[c]['data'][p]['curso']) == str(colocados[r]['data'][t]['curso']):
-                                                            EncontrouColocado = True
-                                                            colocado = "true"
-                                    JSONcontent['opcoes'].append({
-                                        "nome_curso": str(candidatos[c]['data'][p]['curso']),
-                                        "nome_faculdade": str(candidatos[c]['estabelecimento']),
-                                        "cod_curso": str(candidatos[c]['data'][p]['codigo_curso']),
-                                        "cod_faculdade": str(candidatos[c]['codigo_estabelecimento']),
-                                        "nota_candidatura": str(candidatos[c]['data'][p]['candidatos'][i]['nota']),
-                                        "prova_ingresso": str(candidatos[c]['data'][p]['candidatos'][i]['prova_ingresso']),
-                                        "opcao": str(candidatos[c]['data'][p]['candidatos'][i]['opcao']),
-                                        "colocado": str(colocado)
-                                    })
+                                # Todas as pessoas com o meso nome sao inseridas no array MesmoNome
+                                if str(candidatos[x]['data'][j]['candidatos'][k]['nome']) == str(candidatos[c]['data'][p]['candidatos'][i]['nome']):
+                                    MesmoNome.append(Aluno(str(candidatos[c]['data'][p]['candidatos'][i]['nome']), str(candidatos[c]['data'][p]['candidatos'][i]['opcao'])))
+                            # Ver se o array de MesmoNome tem opcoes repetidas
+                            for q in range(len(MesmoNome)):
+                                for w in range(len(MesmoNome)):
+
+
+
+                                    # if str(candidatos[x]['data'][j]['candidatos'][k]['nota_12ano']) == str(candidatos[c]['data'][p]['candidatos'][i]['nota_12ano']):
+                                        # if str(candidatos[x]['data'][j]['candidatos'][k]['nota_10ano_11ano']) == str(candidatos[c]['data'][p]['candidatos'][i]['nota_10ano_11ano']):
+                                            colocado = "false"
+                                            if not EncontrouColocado:
+                                                for r in range(len(colocados)):
+                                                    for t in range(len(colocados[r]['data'])):
+                                                        for y in range(len(colocados[r]['data'][t]['colocados'])):
+                                                            if str(candidatos[x]['data'][j]['candidatos'][k]['nome']) == str(colocados[r]['data'][t]['colocados'][y]['nome']):
+                                                                if str(candidatos[c]['data'][p]['curso']) == str(colocados[r]['data'][t]['curso']):
+                                                                    EncontrouColocado = True
+                                                                    colocado = "true"
+                                            JSONcontent['opcoes'].append({
+                                                "nome_curso": str(candidatos[c]['data'][p]['curso']),
+                                                "nome_faculdade": str(candidatos[c]['estabelecimento']),
+                                                "cod_curso": str(candidatos[c]['data'][p]['codigo_curso']),
+                                                "cod_faculdade": str(candidatos[c]['codigo_estabelecimento']),
+                                                "nota_candidatura": str(candidatos[c]['data'][p]['candidatos'][i]['nota']),
+                                                "prova_ingresso": str(candidatos[c]['data'][p]['candidatos'][i]['prova_ingresso']),
+                                                "opcao": str(candidatos[c]['data'][p]['candidatos'][i]['opcao']),
+                                                "colocado": str(colocado)
+                                            })
                     AlunosTerminados.append(Aluno(str(candidatos[x]['data'][j]['candidatos'][k]['nome']), str(candidatos[x]['data'][j]['candidatos'][k]['nota_12ano']), str(candidatos[x]['data'][j]['candidatos'][k]['nota_10ano_11ano'])))
                     JSONarray.append(JSONcontent)
     with open(r'C:\Users\diogo\Desktop\final.json', 'w') as f:
